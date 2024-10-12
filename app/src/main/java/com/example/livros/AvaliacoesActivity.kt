329911de6ed754a75b6f6bba5b45db867f44bc35
atila-rocha <atilacarvalhorocha@hotmail.com>
2024-10-12 18:31:45 -0300

    Adição da tela de avaliações e integração da tela com o app

--- app/src/main/java/com/example/livros/AvaliacoesActivity.kt
+++ app/src/main/java/com/example/livros/AvaliacoesActivity.kt
@@ -1,27 +1,20 @@
 package com.example.livros
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class ProfileActivity : AppCompatActivity() {
+class AvaliacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_avaliacoes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val imageview2: FloatingActionButton =findViewById(R.id.floatingActionButton)
-        imageview2.setOnClickListener {
-            val intentex = Intent(this, ProfileActivity::class.java)
-            startActivity(intentex)
         }
     }
 }
