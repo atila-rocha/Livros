ad6de606003fbdca232f03f5265de56bbaae279c
atila-rocha <atilacarvalhorocha@hotmail.com>
2024-10-12 16:30:58 -0300

    Alteração na man activity: floating acton button direciona para a tela do usuário. adicionada a tela de usuário completa

--- app/src/main/java/com/example/livros/ProfileActivity.kt
+++ app/src/main/java/com/example/livros/ProfileActivity.kt
@@ -1,29 +1,20 @@
 package com.example.livros
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        //informacoesLivro1.text = "Arquitetura de software: as partes difíceis"
-        val imageview2:FloatingActionButton=findViewById(R.id.floatingActionButton)
-        imageview2.setOnClickListener {
-            val intentex = Intent(this, ProfileActivity::class.java)
-            startActivity(intentex)
         }
     }
 }
